55c9dc3c642fb998d39c6af80a0a14b2b9394d82
Vasilyu <something@email.com>
2019-02-20 12:20:30 +0200

    Created a new project

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
+import VueResource from "vue-resource";
 import App from './App.vue'
-import router from './router'
-import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(VueResource);
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
